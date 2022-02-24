471fd539e89780ebf6cb6e3b62e6cc24dd950ed9
Richard Kim <Richard.kim.0000@gmail.com>
2022-02-24 02:19:53 -0800

    trying to fix postcard.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,8 +16,8 @@ const Home = ({ posts }) => {
       <FeaturedPosts />
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
         <div className="col-span-1 lg:col-span-8">
-          {posts.map((post) => (
-            <PostCard post={post.node} key={post.title} />
+          {posts.map((post, index) => (
+            <PostCard post={post.node} key={index} />
           ))}
         </div>
         <div className="col-span-1 lg:col-span-4">
